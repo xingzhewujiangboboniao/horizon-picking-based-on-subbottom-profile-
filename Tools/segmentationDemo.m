7bbd47656cc17f46b92fd61616af599b56270bef
xingzhewujiangboboniao <51162245+xingzhewujiangboboniao@users.noreply.github.com>
2019-10-21 08:22:03 +0800

    Add files via upload
    1. read data.(envelop data or fullwave data.). Test data can be found through https://zenodo.org/record/3514447#.Xaz53mZ5taQ and https://zenodo.org/record/1482324#.Xaz1-GZ5taR.
    2. .fig provide a GUI to run the software.
    3. The value saved in effect_range.txt  repersents the percentage of the number of rows of data to be processed.
    4. EnvTh.txt represents the envelop threshold in envelop picking.
    5. info.txt saves the storage path of the data.
    6. The value saved in Remove.txt represents the mean distance between two layers.
    7. The value saved in Seg.txt determines whether the texture is segmented or not.
    8. The value saved in Thickness.txt can be set as 3 when a thick layer exists, or set as 2 when a thin layer exists.

---
+++ Tools/segmentationDemo.m
@@ -0,0 +1,39 @@
+%% segmentationDemo
+cN2 = double(cN2);
+inImg = double(inImg);
+nClusters = 2;   
+Size_ = 5;
+winDims = [Size_, Size_];     
+winStep = [floor(Size_/2), floor(Size_/2)];       
+isGrayScale = true;    
+imgSegmentedLBP = segmentationLBP(inImg, nClusters, winDims, winStep, isGrayScale);
+
+[line, row] = size(inImg);
+Segimage = imresize(imgSegmentedLBP,size(inImg));
+reorderedData = reshape(Segimage, [line*row,1]);
+idxCluster = k_means(reorderedData, 2);
+Segimage = reshape(idxCluster, line, row);
+Segimage = switchMatrixlabels(Segimage);
+
+A = sum(sum(cN2(find(Segimage == 1))));
+A = A/length(find(Segimage == 1));
+B = sum(sum(cN2(find(Segimage == 2))));
+B = B/length(find(Segimage == 2));
+if (A>B)
+    Segimage(find(Segimage == 1)) = 255;
+    Segimage(find(Segimage == 2)) = 0;
+else
+    Segimage(find(Segimage == 1)) = 0;
+    Segimage(find(Segimage == 2)) = 255;
+end
+
+
+
+
+
+
+
+
+
+
+      
